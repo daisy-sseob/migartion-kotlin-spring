ed96f655618ba2d3d9099fe41dc41183d63972e2
들국화 <orolsyeo@gmail.com>
2025-03-06 13:03:07 +0900

    Feature/2025 03 06 - userloan history apply querydsl (#13)
    * feat: bookRepository JPQA to QueryDsl
    
    * feat: UserLoanHistoryRepository queryDsl 적용

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -3,12 +3,13 @@ package com.group.libraryapp.service.book
 import com.group.libraryapp.domain.book.Book
 import com.group.libraryapp.domain.book.BookRepository
 import com.group.libraryapp.domain.user.UserJpaRepository
-import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
+import com.group.libraryapp.repository.book.BookQuerydslRepository
+import com.group.libraryapp.repository.user.loanhistory.UserLoanHistoryQuerydslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,8 +17,9 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class BookService(
   private val bookRepository: BookRepository,
+  private val bookQuerydslRepository: BookQuerydslRepository,
   private val userJpaRepository: UserJpaRepository,
-  private val userLoanHistoryRepository: UserLoanHistoryRepository,
+  private val userLoanHistoryRepository: UserLoanHistoryQuerydslRepository,
 ) {
 
   @Transactional
@@ -31,7 +33,7 @@ class BookService(
 
     val book = bookRepository.findByName(request.bookName) ?: fail()
 
-    if (userLoanHistoryRepository.findByBookNameAndStatus(request.bookName, UserLoanStatus.LOANED) != null) {
+    if (userLoanHistoryRepository.find(request.bookName, UserLoanStatus.LOANED) != null) {
       throw IllegalArgumentException("진작 대출되어 있는 책입니다")
     }
 
@@ -48,12 +50,12 @@ class BookService(
 
   @Transactional(readOnly = true)
   fun countLoanedBook(): Int {
-    return userLoanHistoryRepository.countByStatus(UserLoanStatus.LOANED).toInt()
+    return userLoanHistoryRepository.count(UserLoanStatus.LOANED).toInt()
   }
 
   @Transactional(readOnly = true)
   fun getBookStatistics(): List<BookStatResponse> {
-    return bookRepository.getStats()
+    return bookQuerydslRepository.getStats()
   }
 
 
